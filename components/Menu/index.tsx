ead978628591beec63dbec00a073542bb3b7e96a
Elochukwu3 <oguajuromauld5@gmail.com>
2023-10-19 20:30:20 +0100

    update: tw styles for buttons and menu

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,3 +1,4 @@
+import { ButtonWrapper } from "@components/Button/ButtonStyle"
 import {Menu as  MenuWrappper, MenuItem} from "./MenuStyle"
 
 
@@ -8,6 +9,7 @@ const Menu = () => {
       <MenuItem>Location</MenuItem>
       <MenuItem>services</MenuItem>
       <MenuItem>sign up/sign in</MenuItem>
+      <ButtonWrapper>Become Mender</ButtonWrapper>
     </MenuWrappper>
   )
 }
--- components/Nav/NavStyle.ts
+++ components/Nav/NavStyle.ts
@@ -2,8 +2,8 @@ import tw from "tailwind-styled-components";
 
 
 const NavWrapper = tw.div `
-flex justify-between w-full items-center
-bg-white shadow-md shadow-green-50 
+flex justify-between whitespace-nowrap items-center
+bg-white shadow-md shadow-green-50 break-inside-avoid w-auto
 p-6
 `
 export {NavWrapper}
