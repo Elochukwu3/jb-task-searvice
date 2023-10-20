8e10ff16de028bca13957a0e91217b19f3f11d5a
Elochukwu3 <oguajuromauld5@gmail.com>
2023-10-20 09:38:51 +0100

    clean up

--- components/Menu/DropDown.tsx
+++ components/Menu/DropDown.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useState } from "react";
 const DropDown = () => {
   const list = [
     "Furniture Assembly",
@@ -14,17 +13,48 @@ const DropDown = () => {
     "Hang Pictures",
     "Lighting Installation",
   ];
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
   return (
-    <ul
-      className='absolute before:border-t font-satoshi font-semibold before:border-b shadow-md space-y-5 text-xl capitalize beforeborder-slate-400 before:content-[""] before:block before:w-[69%] before:h- origin-bottom-left 
-    bg-red-70 w-80 right-[0.5%] top-[90%] border-x border-b  border-slate-400'
-    >
-        <div  className="w-8/12 mx-auto block opacity-80 ">  Popular Tasks</div>
-      {list.map((item) => (
-        <div key={item} className="w-8/12 mx-auto block text-[#022022]">{item}</div>
-      ))}
-    </ul>
+    <>
+      {isDropdownOpen && (
+        <ul
+          className='hidden absolute before:border-t font-satoshi font-semibold before:border-b shadow-md space-y-4 text-base capitalize beforeborder-slate-400 before:content-[""] before:block before:w-[69%] before:h- origin-bottom-left 
+    bg-red-70 w-80 right-[0.5%] top-[90%] border-x border-b bg-white  border-slate-400'
+        >
+          <div className="w-8/12 mx-auto block opacity-70 text-zinc-500  pt-4">
+            {" "}
+            Popular Tasks
+          </div>
+          {list.map((item) => (
+            <div
+              key={item}
+              className="w-8/12 mx-auto block text-custom-greaner"
+            >
+              {item}
+            </div>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
 export default DropDown;
+
+
+// const dropdownRef = useRef(null);
+
+//   const handleDocumentClick = (e) => {
+//     if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+//       setDropdownOpen(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     document.addEventListener("click", handleDocumentClick);
+//     return () => {
+//       document.removeEventListener("click", handleDocumentClick);
+//     };
+//   }, []);
+
+
--- components/Menu/MenuStyle.tsx
+++ components/Menu/MenuStyle.tsx
@@ -2,14 +2,16 @@ import tw from "tailwind-styled-components";
 import { ButtonProp } from "@components/Button/ButtonStyle";
 
 const Menu = tw.nav`sm:flex justify-between
-gap-4 font-unical text-lg items-center
+gap-4 satoshi text-lg items-center
 max-lg:text-sm max-sm:hidden
 `;
 
 const MenuItem = tw.span<ButtonProp>`
-capitalize font-medium max-lg:font-normal 
-    block px-2 py- before:content-[""] transition-all before:duration-300 
-    before:ease-in-out before:w-0 before:h-1 before:absolute before:right-0 before:left-0 before:bg-black
-    ${(p)=>(p.$bool && "hover:bg-white hover:shadow-md hover:before:w-full border-slate-600 relative hover:border-x border-slate-400")}
+capitalize font-medium max-lg:font-normal px-2  
+    block py- before:content-[""] transition-all before:duration-300 
+    before:ease-in-out before:w-0 before:h-1 before:absolute before:right-0 before:left-0  before:bg-black
+    ${(p)=>(p.$bool && "group hover:bg-white hover:shadow-md hover:before:w-full relative  border-slate-400")}
     `;
 export { Menu, MenuItem };
+
+
