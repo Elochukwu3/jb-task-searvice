d95582dbfd478b10e550fdef802a573d5149b334
Elochukwu3 <oguajuromauld5@gmail.com>
2023-11-01 15:50:50 +0100

    built app

---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>page not NotFound</div>;
+};
+
+export default NotFound;
--- components/Home/Banner.tsx
+++ components/Home/Banner.tsx
@@ -3,7 +3,9 @@ import {BannerWrapper} from "./homeStyle"
 
 const Banner = () => {
   return (
-    <BannerWrapper  className="main">
+    <BannerWrapper  className="main"
+    style={{backgroundImage: `url(${"/images/banner-1.jpeg"})`}}
+    >
         <SearchSection/>
     </BannerWrapper>
   )
