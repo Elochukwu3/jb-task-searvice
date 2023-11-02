c01c97acfaa6a0fd5c7a09b754ec33ecf661c1b1
Elochukwu3 <oguajuromauld5@gmail.com>
2023-11-02 21:45:19 +0100

    update: styles for earch section

--- components/Home/homeStyle.ts
+++ components/Home/homeStyle.ts
@@ -4,7 +4,7 @@ bg-no-repeat bg-cover bg-center w-full
 h-[495px] flex justify-center items-center
 bg-[#f6f5f1] aspect-square overflow-hidden
 `;
-
+//search
 const SearchBoldText = tw.span`max-sm:text-l block text-custom-dark text-4xl text-center 
 font-bold whitespace-nowrap max-md:text-3xl `;
 const SearchSectionWrapper = tw.div`bg-white z-50 p-4 rounded-md max-md:w-11/12 w-7/12  max-sm:w-full`;
