d69e1be0fc9e33c7caaabbc21bacbcba3a53ad32
Elochukwu3 <oguajuromauld5@gmail.com>
2023-11-18 12:10:08 +0100

    bug: animation

--- components/product-card/Testimonial.tsx
+++ components/product-card/Testimonial.tsx
@@ -1,22 +1,42 @@
 import useDropDown from "@hooks/useDropDown";
- import {motion } from "framer-motion";
+ import {motion, AnimatePresence } from "framer-motion";
+
 
 
 const Testimonial = () => {
-  const vairant={
-      
+  const variant={
+    initial:{
+      transition:{
+        delay: 2.279,
+        ease: [0.83, 0, 0.17, 1],
+        duration: 2,
+      },
+      scaleY:.9,
+      origin: "center"
+    },
+    enter:{
+      scaleY: 1,
+      transition:{
+        delay: 2.279,
+        ease: [0.83, 0, 0.17, 1],
+        duration: 2,
+      },
+    },
+    exit:{
+
+    }
   }
   return (
       <details className="text-sm my-5 px-5 ">
         <summary className="inline-flex justify-between">
           A Click, A Fix, A Wow – Thank You, JPPromend!<span className="after:content-['+']"></span>
         </summary>
-        <div className="">
-        <div className="bg-custom-greaner p-4 origin-center ease-[cubic-bezier(0.83, 0, 0.17, 1)]">
+        < AnimatePresence mode="wait">
+        <motion.div variants={variant} initial="initial" animate="enter" className="text-white bg-custom-greaner p-4 origin-center ease-">
           Can I work on a project I started before the hackathon? Can I work on
           a project I started before the hackathon
-        </div>
-        </div>
+        </motion.div>
+        </ AnimatePresence>
       </details>
   );
 };
