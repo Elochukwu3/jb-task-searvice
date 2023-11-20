ade61cb3e0eaf5c6496714c670fa2aca2b55d7a5
Elochukwu3 <oguajuromauld5@gmail.com>
2023-11-20 19:16:08 +0100

    fixing dropdown animation

--- components/product-card/Testimonial.tsx
+++ components/product-card/Testimonial.tsx
@@ -7,37 +7,68 @@ const Testimonial = () => {
   const variant={
     initial:{
       transition:{
-        delay: 2.279,
         ease: [0.83, 0, 0.17, 1],
-        duration: 2,
+        bounce: 0.4,
+        type:"spring",
       },
-      scaleY:.9,
+      scaleY:0,
       origin: "center"
     },
     enter:{
       scaleY: 1,
       transition:{
-        delay: 2.279,
         ease: [0.83, 0, 0.17, 1],
-        duration: 2,
+        bounce: 0.4,
+        type:"spring",
+        // duration: 2,
       },
     },
     exit:{
 
     }
   }
+  const height = 200;
+  const borderheight = `calc(${height}px * 0.05)`;
+  const trangleVar= {
+    initial:{
+        borderColor: 'red',
+        borderBottomColor: 'transparent',
+        borderStyle: 'solid',
+        borderTopWidth: borderheight,
+        borderTopColor: 'transparent',
+         borderRightColor: 'white',
+      borderRightWidth: 0,
+
+    },
+    enter:{
+      borderRightColor: 'white',
+      borderRightWidth: 0,
+      borderBottomColor: 'transparent',
+      borderStyle: 'solid',
+      borderBottomWidth: borderheight,
+      borderTopWidth: borderheight,
+      borderTopColor: 'transparent',
+    }
+  }
+ const {isDropdownOpen,  handleDropDown}= useDropDown()
+
   return (
-      <details className="text-sm my-5 px-5 ">
-        <summary className="inline-flex justify-between">
+      <div className="text-sm my-5 px- ">
+        <p onClick={handleDropDown} className="inline-flex justify-between">
           A Click, A Fix, A Wow – Thank You, JPPromend!<span className="after:content-['+']"></span>
-        </summary>
-        < AnimatePresence mode="sync" initial={false}>
-        <motion.div variants={variant} initial="initial" animate="enter" className="text-white bg-custom-greaner p-4 origin-center ease-">
+        </p>
+        < AnimatePresence mode="sync" initial={false} >
+        {isDropdownOpen &&
+        <motion.div variants={variant} exit="initial" initial="initial" animate="enter" className="relative text-white bg-custom-greaner p-4 origin-center ">
+        <motion.div variants={trangleVar} exit={"initial"} animate="enter"
+         className="trapezium2 h-0 w-1/4 right-0 absolute top-1/2 -translate-y-1/2"/>
+          <motion.div>
           Can I work on a project I started before the hackathon? Can I work on
           a project I started before the hackathon
-        </motion.div>
+          </motion.div>
+        </motion.div>}
         </ AnimatePresence>
-      </details>
+      </div>
   );
 };
 export default Testimonial
