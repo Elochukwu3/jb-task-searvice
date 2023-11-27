36a183daa5ed87b2d8a4744226b889079541093a
Elochukwu3 <oguajuromauld5@gmail.com>
2023-11-27 02:02:09 +0100

    clean up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,18 @@
 const Footer = () => {
   
     return (
-      <div className=" font-montserrat text-white mt-14 bg-footer py-10 mb-0 relative">
-           <span className="text-xl left-14 max-sm:left-4 max-sm:inline max-sm:text-sm text-white top-1/4 max-sm:top-20 max-md:hidden block  absolute z-20">
+      <div className="bg-custom-greaner/60 font-montserrat text-custom-dark mt-14 bg-footer py-10 mb-0 relative">
+           <span className="text-xl left-14 max-sm:left-4 max-sm:inline max-sm:text-sm text-custom-dark top-1/4 max-sm:top-20 max-md:hidden block  absolute z-20">
                 &#x2726;
               </span>
-        <div className=" w-10/12 max-sm:w-full max-sm:pl-10 mx-auto grid grid-cols-4 mb-5 max-md:grid-cols-1 max-sm:space-y-12">
+        <div className=" w-10/12 max-sm:w-full max-sm:pl-10 mx-auto grid grid-cols-5 mb-5 max-md:grid-cols-1 max-sm:space-y-12">
           <div className="col-span-2 max-sm:col-span-1 pr-12 max-md:pr-0">
             <div className="font-clash text-3xl font-bold my-6 ">
-              <span>get</span>
-              <span className="text-custom-highlight">linked</span>
+            JBProMend
             </div>
             <p className="flex flex-col gap-10 max-sm:text-sm pr-3">
               <span>
-              Getlinked Tech Hackathon is a technology innovation program
+              JBProMend is a technology innovation program
               established by a group of organizations with the aim of showcasing
               young and talented individuals in the field of technology
               </span>
@@ -48,6 +47,20 @@ const Footer = () => {
                   <img src={ICON.linked} alt="" />
                 </LinkRoute>
               </div> */}
+            </div>
+          </div>
+          <div className="space-y-5 relative">
+               <span className="text-xl right-10 max-sm:text-sm text-zinc-600 bottom-3/4 max-md:bottom-1/2  absolute z-20">
+                &#x2726;
+              </span>
+            <p className="text-custom-highlight">Popular Locations</p>
+            <p className="block cursor-pointer" >Enugu</p>
+            <p  className="block cursor-pointer">Lagos</p>
+            <p  className="block cursor-pointer" >Anambra</p>
+            <p  className="block cursor-pointer">Abia</p>
+            <p  className="block cursor-pointer">Ebonyi</p>
+            <div className="flex gap-4 items-center">
+              <p className="text-custom-highlight">Follow us</p>
             </div>
           </div>
           <div className="space-y-5 relative">
