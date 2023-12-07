4db92eaddcbe957c8ac846ea68267f5be575a93e
Elochukwu3 <oguajuromauld5@gmail.com>
2023-12-07 06:03:20 +0100

    Trust rating

---
+++ components/Home/rating-home/TrustText.tsx
@@ -0,0 +1,29 @@
+import {Liwrapper, TrustTextWrapper} from "./style";
+import {rating_text} from "./ratiingData"
+
+
+
+const TrustText = ()=>{
+    return(
+        <TrustTextWrapper>
+            <h2>Trust and safety features for your protection</h2>
+            {/* {rating-tex} */}
+            <Liwrapper>
+            <svg width="24" height="24" viewBox="0 0 24 24">
+            
+            </svg>
+                <div>
+                    <p>Secure payments</p>
+                    <p>Only release payment when the task is completed to your satisfaction</p>
+                    <span>read  more</span>
+                </div>
+            </Liwrapper>
+        </TrustTextWrapper>
+    )
+}
+export default TrustText;
+{/* < width="24" height="24" viewBox="0 0 24 24">
+<pa fill-rule="evenodd" d="m12 15.925-3.15 1.9.825-3.575L6.9 11.825l3.65-.325L12 8.125l1.45 3.4 3.65.325-2.775 2.4.825 3.6L12 15.925Zm-4.55-.95L5.825 22 12 18.275 18.175 22l-1.625-7.025L22 10.25l-7.2-.625L12 3 9.2 9.625 2 10.25l5.45 4.725Z" clip-rule="evenodd"/> */}
+{/* <svg width="24" height="24" viewBox="0 0 24 24">
+<mask id="a" width="24" height="24" x="0" y="0" maskUnits="userSpaceOnUse" style="mask-type:alpha"><path d="M0 0h24v24H0z"></path></mask><g mask="url(#a)"><path d="M10.95 15.55 16.6 9.9l-1.425-1.425L10.95 12.7l-2.1-2.1-1.425 1.425 3.525 3.525ZM12 22c-2.317-.583-4.229-1.913-5.737-3.988C4.754 15.937 4 13.633 4 11.1V5l8-3 8 3v6.1c0 2.533-.754 4.837-2.262 6.912C16.229 20.087 14.317 21.417 12 22Zm0-2.1c1.733-.55 3.167-1.65 4.3-3.3s1.7-3.483 1.7-5.5V6.375l-6-2.25-6 2.25V11.1c0 2.017.567 3.85 1.7 5.5s2.567 2.75 4.3 3.3Z"></path></g>
+</svg> */}
---
+++ components/Home/rating-home/style.ts
@@ -0,0 +1,5 @@
+import tw from "tailwind-styled-components";
+
+const TrustTextWrapper = tw.div`block`
+const Liwrapper = tw.div`flex justify-center gap-7 my-6 items-start`
+export {Liwrapper, TrustTextWrapper}
