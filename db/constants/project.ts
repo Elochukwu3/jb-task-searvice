d7e953661bf0c2149bf1f416a1624f597bf6d1b4
Elochukwu3 <oguajuromauld5@gmail.com>
2023-12-29 17:57:17 +0100

    config: image path and caption array object for mini project card

---
+++ db/constants/project.ts
@@ -0,0 +1,12 @@
+
+export const MINI_PROJECT = [
+    {img: "/project-img/painting.png", header: "Yard Work", caption: "Transform your outdoor space with our yard work services."},
+    {img: "/project-img/furniture-min.png", header: "Furniture Assembly", caption: "Expert assembly for your furniture needs."},
+    {img: "/project-img/painter-min.png", header: "Painting", caption: "Add a splash of color to your walls with our professional painting services."},
+    {img: "/project-img/maintenance.png", header: "Lighting Installation", caption: "Illuminate your space with our efficient lighting installation."},
+    {img: "/project-img/hangperson.png", header: "Handyman", caption: "Fix, repair, and maintain your home with our skilled handyman services."},
+    {img: "/project-img/deliver-min.png", header: "Mounting Services", caption: "Securely mount your items with our reliable mounting services."},
+    {img: "/project-img/cleaning-min.png", header: "Cleaning", caption: "Keep your space spotless with our thorough cleaning services."},
+];
+
+   
