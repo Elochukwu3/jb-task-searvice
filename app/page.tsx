0c93987174cc747b3e1e5ee63c52a9f503cd1610
Elochukwu3 <oguajuromauld5@gmail.com>
2023-12-29 18:01:18 +0100

    ref: app and component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { Metadata } from "next";
 import Banner from "@components/Home/banner-section/Banner";
-import ProductCard from "@components/product-card/Card";
+import ProductCard from "@components/projectCard/Card";
 import MoreDone from "@components/Home/getmoredone";
 import GettingDone from "@components/Home/getdone";
 import HomeRating from "@components/Home/rating-home";
-import ProjectOutline from "@components/Home/banner-section/ProjectOutline";
-import ProjectCard from "@components/project_card";
+import ProjectOutline from "@components/projectCard/ProjectOutline";
+import MiniProjectCard from "@components/project_card/Projects";
 
 export const metadata: Metadata = {
   openGraph: {
@@ -28,21 +28,7 @@ export default function Home() {
   return (
     <div className="items-center flex flex-col justify-center gap-10">
       <Banner />
-
-      <div className=" overflow-hidden slider my-5 whitespace-nowrap flex">
-        <div className="slide-track ">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-        </div>
-        <div className="slide-track">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-        </div>
-      </div>
+      <MiniProjectCard/>
 
       <div className="w-full  gap-3 flex max-lg:flex-col justify-center max-lg:w-[98.1%] mx-auto max-md:items-center my-10">
         <div className="  lg:w-1/4 md:w-10/12 mx-auto max-sm:px-4">
