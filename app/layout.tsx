f8e5d470ed2f2b1a482396d00d1d5b5dfdb1922e
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-03 23:16:54 +0100

    Add: toaster for notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
 import type { Metadata } from "next";
 import "@styles/globals.css";
 import { ReactProp } from "@app-types/type";
+import { ToasterProvider } from "@components/providers/ToasterProvider";
 
-// export const metadata: Metadata = {
-//   title: "JBProMend",
-//   description:
-//     "JBProMend: Find Trusted Taskers Near You | Local Services Made Easy",
-// };
+export const metadata: Metadata = {
+  title: "JBProMend",
+  description:
+    "JBProMend: Find Trusted Taskers Near You | Local Services Made Easy",
+};
 
 export default function RootLayout({ children }: ReactProp) {
   return (
     <html lang="en">
       <body>
-          <div>
+          <ToasterProvider/>
             {children}
-          </div>
       </body>
     </html>
   );
