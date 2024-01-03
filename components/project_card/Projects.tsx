7997190d357c2b194dfb113040431d548f84be93
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-03 23:38:22 +0100

    Add: shadcnui button, form and input

--- components/project_card/Projects.tsx
+++ components/project_card/Projects.tsx
@@ -3,12 +3,13 @@ import { MINI_PROJECT} from "@db/constants/project"
 const Projects = ()=>{
     return(
         <div className=" overflow-hidden slider my-5 whitespace-nowrap flex">
-        <div className="slide-track ">
+          hello
+        {/* <div className="slide-track ">
           { MINI_PROJECT.map((item)=>(<ProjectCard {...item} key={`${item.header}`}/>))}
         </div>
         <div className="slide-track ">
           { MINI_PROJECT.map((item)=>(<ProjectCard {...item} key={`${item.header}`}/>))}
-        </div>
+        </div> */}
   
       </div>
     )
