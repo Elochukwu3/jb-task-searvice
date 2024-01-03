d8e75564b7ed019cad7872c5bcabd7c8a16ba268
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-03 22:23:45 +0100

    fixing shadcnui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import "@styles/globals.css";
 import { ReactProp } from "@app-types/type";
 
-export const metadata: Metadata = {
-  title: "JBProMend",
-  description:
-    "JBProMend: Find Trusted Taskers Near You | Local Services Made Easy",
-};
+// export const metadata: Metadata = {
+//   title: "JBProMend",
+//   description:
+//     "JBProMend: Find Trusted Taskers Near You | Local Services Made Easy",
+// };
 
 export default function RootLayout({ children }: ReactProp) {
   return (
