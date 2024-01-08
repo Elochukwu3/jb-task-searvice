af552e4f94708c99018aed41ed9bafea40bfadb9
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-08 14:31:15 +0100

    add: context for managing single form page state

---
+++ app/(task)/context/FormProvider.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useContext } from 'react';
+import { ReactProp } from "@app-types/type";
+
+type SidebarContextType = {
+  activeTab: string;
+  handleSetter: (value: string) => void;
+};
+
+const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
+
+export const FormProvider: React.FC = ({ children }:ReactProp ) => {
+  const [activeTab, setActiveTab] = useState<string>('title');
+const handleSetter = (value:string)=>{
+    setActiveTab(value)
+}
+  return (
+    <SidebarContext.Provider value={{ activeTab, handleSetter }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+ const useSidebarContext = (): SidebarContextType => {
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error('useSidebarContext must be used within a SidebarProvider');
+  }
+  return context;
+};
+
+export default useSidebarContext;
