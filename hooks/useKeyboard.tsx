8daa5509d1e89d0035b0b738a88717cb6afac637
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-10 23:48:39 +0100

    Add: keyboard keys custom hook for easy selection

---
+++ hooks/useKeyboard.tsx
@@ -0,0 +1,74 @@
+import { FC, useCallback, useEffect, useRef, useState } from "react";
+
+interface Props<T> {
+  results?: T[];
+  onSelect?: (item: T) => void;
+  callback?: (param:string, item:T)=> void;
+}
+
+const useKeyboardKey = <T extends object>({
+  results = [],
+  onSelect,
+}: Props<T>) => {
+  const [focusedIndex, setFocusedIndex] = useState(-1);
+  const resultContainer = useRef<HTMLDivElement>(null);
+  const [showResults, setShowResults] = useState(false);
+
+  const handleSelection = (selectedIndex: number) => {
+    const selectedItem = results[selectedIndex];
+    if (!selectedItem) return resetSearchComplete();
+    onSelect && onSelect(selectedItem);
+    resetSearchComplete();
+    // callback("search", selectedItem);
+  };
+
+  const resetSearchComplete = useCallback(() => {
+    setFocusedIndex(-1);
+    setShowResults(false);
+  }, []);
+
+  const handleKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
+    const { key } = e;
+    let nextIndexCount = 0;
+
+    // move down
+    if (key === "ArrowDown")
+      nextIndexCount = (focusedIndex + 1) % results.length;
+
+    // move up
+    if (key === "ArrowUp")
+      nextIndexCount = (focusedIndex + results.length - 1) % results.length;
+
+    // hide search results
+    if (key === "Escape") {
+      resetSearchComplete();
+    }
+
+    // select the current item
+    if (key === "Enter") {
+      e.preventDefault();
+      handleSelection(focusedIndex);
+    }
+
+    setFocusedIndex(nextIndexCount);
+  };
+
+  useEffect(() => {
+    if (!resultContainer.current) return;
+
+    resultContainer.current.scrollIntoView({
+      block: "center",
+    });
+  }, [focusedIndex]);
+
+  useEffect(() => {
+    if (results.length > 0 && !showResults) setShowResults(true);
+
+    if (results.length <= 0) setShowResults(false);
+  }, [results]);
+
+
+  return {showResults, resetSearchComplete, handleKeyDown}
+};
+
+export default useKeyboardKey;
