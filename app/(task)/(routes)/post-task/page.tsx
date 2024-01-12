500be07c41354edcd275cfbd543a736d8a93f321
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-12 23:16:58 +0100

    update: post task page

--- app/(task)/(routes)/post-task/page.tsx
+++ app/(task)/(routes)/post-task/page.tsx
@@ -1,45 +1,52 @@
-"use client";
+"use client"
 import { Metadata } from "next";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CreateTitle from "./_title/page";
 import CreateLocation from "./_location/page";
 import Details from "./_details/page";
 import Budget from "./_budget/page";
 import useSidebarContext from "@app/(task)/context/FormProvider";
 import { useSearchParams } from 'next/navigation';
-import {convertToObject} from "./helper/convertArray"
+import { convertToObject } from "./helper/convertArray";
 
-
-
- const metadata: Metadata = {
+const metadata: Metadata = {
   title: "New task",
-  description: "ask about our product",
+  description: "Ask about our product",
 };
 
 const Page = () => {
-  const { activeTab} = useSidebarContext();
+  const { activeTab } = useSidebarContext();
   const searchParams = useSearchParams();
   const param = searchParams.get('origin');
+  const [loading, setLoading] = useState(true);
+  const [content, setContent] = useState<JSX.Element | null>(null);
 
-  let content: JSX.Element;
-try {
-  const routes = convertToObject();
-  if (param === routes?.titleDate) {
-    content = <CreateTitle />;
-  } else if (param === routes?.Location) {
-    content = <CreateLocation/>;
-  } else if (param === routes?.Details ) {
-    content = <Details />;
-  } else if (param === routes?.Budget ) {
-    content = <Budget/>;
-  } else {
-    content = <div>content not available</div>;
-  }
-} catch (error) {
-  content = <div>Error loading content</div>;
-}
+  useEffect(() => {
+    const loadContent = async () => {
+      try {
+        const routes = convertToObject();
+        if (param === routes?.titleDate) {
+          setContent(<CreateTitle />);
+        } else if (param === routes?.Location) {
+          setContent(<CreateLocation />);
+        } else if (param === routes?.Details) {
+          setContent(<Details />);
+        } else if (param === routes?.Budget) {
+          setContent(<Budget />);
+        } else {
+          setContent(<div>Content not available</div>);
+        }
+      } catch (error) {
+        setContent(<div>Error loading content</div>);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  return param && content;
+    loadContent();
+  }, [param]);
+
+  return loading ? <div>Loading...</div> : content;
 };
 
 export default Page;
