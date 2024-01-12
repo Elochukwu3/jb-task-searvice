2f4bea37b2207e5b37a008c04539ca833dc4dcb4
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-12 11:18:36 +0100

    clean up

--- app/(task)/(routes)/post-task/_location/page.tsx
+++ app/(task)/(routes)/post-task/_location/page.tsx
@@ -16,13 +16,13 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { debounceFunc } from "@utils/debounce";
 import { useState } from "react";
-import useKeyboardKey from "@hooks/useKeyboard"
+import useKeyboardKey from "@hooks/useKeyboard";
 
-export const revalidate = 10
+export const revalidate = 10;
 const CreateLocation = () => {
   const [data, setData] = useState<any[]>([]);
   const [err, setErr] = useState<string>();
-  const [selected, setSelected] = useState<object>({})
+  const [selected, setSelected] = useState<object>({});
   const inputSchema = object({
     search: string().min(1, {
       message: "Title is required",
@@ -35,15 +35,24 @@ const CreateLocation = () => {
       search: "",
     },
   });
-  const {focusedIndex, resultContainer, showResults, resetSearchComplete, handleKeyDown} = useKeyboardKey({results: data,
+  const {
+    focusedIndex,
+    resultContainer,
+    showResults,
+    resetSearchComplete,
+    handleKeyDown,
+  } = useKeyboardKey({
+    results: data,
     onSelect: (item) => setSelected(item),
     onSearchSubmit: (searchQuery) => form.setValue("search", searchQuery),
-  })
+    resetResult: (item)=> setData(item)
+  });
 
   const debouncedSearch = debounceFunc(async (searchQuery: string) => {
     try {
       const response: AxiosResponse<any> = await axios.get(
-        `https://nominatim.openstreetmap.org/search?q=${searchQuery}&format=json&limit=6&countrycodes=NG`);
+        `https://nominatim.openstreetmap.org/search?q=${searchQuery}&format=json&limit=6&countrycodes=NG`
+      );
       setData(response.data);
     } catch (error) {
       setErr("Error fetching search data:");
@@ -51,24 +60,22 @@ const CreateLocation = () => {
   }, 300);
   const handleSelectItem = (selectedValue: string) => {
     form.setValue("search", selectedValue);
-    setData([]); 
+    setData([]);
   };
 
   return (
     <div className="max-w-5xl mx-auto flex  md:justify-center h-full p-6">
       <div className="w-full">
-        <h1 className="text-3xl text-custom-dark">
-          Your Location
-        </h1>
+        <h1 className="text-3xl text-custom-dark">Your Location</h1>
         <section className="grid grid-cols-2 w-full text-sm">
-        <div className="w-11/12 text-center p-3 rounded-sm bg-custom-dark text-white">
-<p>In-person</p>
-<p>Select this if you need the Tasker physically there</p>
-        </div>
-        <div className="w-11/12 text-center p-3 rounded-sm bg-slate-300 text-custom-dark">
-<p>In-person</p>
-<p>Select this if you need the Tasker physically there</p>
-        </div>
+          <div className="w-11/12 text-center p-3 rounded-sm bg-custom-dark text-white">
+            <p>In-person</p>
+            <p>Select this if you need the Tasker physically there</p>
+          </div>
+          <div className="w-11/12 text-center p-3 rounded-sm bg-slate-300 text-custom-dark">
+            <p>In-person</p>
+            <p>Select this if you need the Tasker physically there</p>
+          </div>
         </section>
         <Form {...form}>
           <form className="space-y-8 mt-6">
@@ -76,10 +83,11 @@ const CreateLocation = () => {
               control={form.control}
               name="search"
               render={({ field }) => (
-                <FormItem   
-                tabIndex={1}
-                onBlur={resetSearchComplete}
-                onKeyDown={handleKeyDown}>
+                <FormItem
+                  tabIndex={1}
+                  onBlur={resetSearchComplete}
+                  onKeyDown={handleKeyDown}
+                >
                   <FormLabel className="block text-custom-dark pb-2">
                     Where do you need this done?
                   </FormLabel>
@@ -103,23 +111,27 @@ const CreateLocation = () => {
             {/* <Button className="bg-custom-greaner text-white" type="submit">Submit</Button> */}
           </form>
         </Form>
-        {(err && !data) &&<span className="text-red-400 ">{err}</span>}
-        {data?.length > 0 ? (
-          <div className="">
-            {data.map((item, i) => (
-              <div 
-              key={`select${item?.name + i}`}
-              onClick={() => handleSelectItem(item?.display_name)}
-              ref={resultContainer(i)}
-              style={{
-                backgroundColor:
-                  i === focusedIndex ? "rgba(0,0,0,0.1)" : "",
-              }}
-              className="block w-full   "
-            >{item?.display_name}</div>
-            ))}
+        {showResults && (
+          <div className="bg-white w-full space-y-1 text-custom-dark max-h-40 overflow-y-scroll no-scroll">
+            {data?.length > 0 ? (
+              data.map((item, i) => (
+                <div
+                  key={`select${item?.name + i}`}
+                  onClick={() => handleSelectItem(item?.display_name)}
+                  ref={resultContainer(i)}
+                  className={`${i === focusedIndex ? "bg-green-100 " : ""} block w-full p-1 rounded-sm`}
+                >
+                  {item?.display_name}
+                </div>
+              ))
+            ) : (
+              <span>notfound</span>
+            )}
           </div>
-        ): <span>notfound</span>}
+        )}
+          {!selected && form.formState.touchedFields.search && form.formState.isDirty && !showResults && (
+          <span className="text-red-400 ">Field is required</span>
+        )}
       </div>
     </div>
   );
--- hooks/useKeyboard.tsx
+++ hooks/useKeyboard.tsx
@@ -4,12 +4,14 @@ interface Props<T> {
   results?: T[];
   onSelect?: (item: T) => void;
 onSearchSubmit?: (searchQuery: string) => void;
+resetResult?: (item: Array<T>)=> void
 }
 
 const useKeyboardKey = <T extends {display_name: string }>({
   results = [],
   onSelect,
-  onSearchSubmit
+  onSearchSubmit,
+  resetResult
 }: Props<T>) => {
   const [focusedIndex, setFocusedIndex] = useState(-1);
   const resultContainer = useRef<HTMLDivElement>(null);
@@ -19,34 +21,32 @@ const useKeyboardKey = <T extends {display_name: string }>({
     const selectedItem = results[selectedIndex];
     if (!selectedItem) return resetSearchComplete();
     onSelect && onSelect(selectedItem);
-    resetSearchComplete();
     onSearchSubmit && onSearchSubmit(selectedItem.display_name)
-    console.log(selectedItem, "selected")
+    resetSearchComplete();
+
+   
   };
 
   const resetSearchComplete = useCallback(() => {
     setFocusedIndex(-1);
     setShowResults(false);
+    resetResult && resetResult([])
   }, []);
 
   const handleKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
     const { key } = e;
     let nextIndexCount = 0;
 
-    // move down
     if (key === "ArrowDown")
       nextIndexCount = (focusedIndex + 1) % results.length;
 
-    // move up
     if (key === "ArrowUp")
       nextIndexCount = (focusedIndex + results.length - 1) % results.length;
 
-    // hide search results
     if (key === "Escape") {
       resetSearchComplete();
     }
 
-    // select the current item
     if (key === "Enter") {
       e.preventDefault();
       handleSelection(focusedIndex);
