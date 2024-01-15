1f498758d390068fe66db7f9412de3f81421c2da
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-15 22:14:13 +0100

    fix errror

--- app/(task)/_components/SideBarItem.tsx
+++ app/(task)/_components/SideBarItem.tsx
@@ -17,15 +17,15 @@ const SideBarItem = ({ label, href }: SidebarItemProps) => {
   const router = useRouter()
   const{activeTab} = useSidebarContext (); 
   const handleClick = (e:React.MouseEvent<HTMLButtonElement>) => {
-    console.log(activeTab[href], e, "out")
-    if(activeTab[href]){
-      console.log(activeTab[href], e)
+    // console.log(activeTab[href], e, "out")
+    // if(activeTab[href]){
+    //   console.log(activeTab[href], e)
+    // }
       router.push(`/post-task?origin=${href}`, {scroll: false});
-    }
   };
   return (
-    <BarButton id={href} disabled={activeTab[href]} className="cursor-pointer" $isActive={param === href} onClick={handleClick}>
-      <span className=" text-custom-greaner">{label}</span>
+    <BarButton id={href} disabled={!activeTab[href]} className="cursor-pointer" $isActive={param === href} onClick={handleClick}>
+      <span className="disabled:text-red-700 text-custom-greaner">{label}</span>
       <span
         className={cn(
           "absolute inset-y-0 left-0 opacity-0 border-2 block border-custom-greaner h-full transition-all",
