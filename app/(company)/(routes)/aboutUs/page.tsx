d2f6a779905d89d35c15f3f3c8f7ec64bbca038e
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-17 23:58:54 +0100

    update: about style

--- app/(company)/(routes)/aboutUs/page.tsx
+++ app/(company)/(routes)/aboutUs/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
     <section className='max-w-7xl mx-auto '>
       <div className='min-h-[60vh] w-11/12 rounded-md mx-auto flex justify-between p-6 bg-[#F9F3EF] '>
       <div>
-        <h1>We&apos;`re making a difference to people&apos;`s lives</h1>
+        <h1 className='text-4xl'>We&apos;`re making a difference to people&apos;`s lives</h1>
         <p>Creating a way to connect people ready to work, with people who need work done</p>
       </div>
       <div className="flex-1">
