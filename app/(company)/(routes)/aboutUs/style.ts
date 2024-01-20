006ba6a04c834a121d9e7d9cb610799d66bb694b
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-20 17:29:00 +0100

    Add: component to about tailwind style for component connecter

--- app/(company)/(routes)/aboutUs/style.ts
+++ app/(company)/(routes)/aboutUs/style.ts
@@ -8,4 +8,5 @@ import { cn } from "@/lib/utils"
 const AboutLi = tw.li`mb-4 flex items-center justify-between 
 `;
 
-export { AboutLi};
+const AboutConnector = tw.div`relative z-1 md:w-[8%] flex max-md:flex-col items-start h-fit md:mt-14 md:items-center`
+export { AboutLi, AboutConnector };
