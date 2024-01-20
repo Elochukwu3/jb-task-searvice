91cf4c3c4ce243b56a5a82d66007eb815f3b8271
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-20 17:27:41 +0100

    update:  about component styles

--- app/(company)/(routes)/aboutUs/components/Comp.tsx
+++ app/(company)/(routes)/aboutUs/components/Comp.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { AboutLi } from "../style";
+import { AboutConnector, AboutLi } from "../style";
 import Svg from "./Svg";
-
-const Comp = () => {
+import { cn } from "@lib/utils";
+const Comp = ({reverse}:{reverse?: boolean}) => {
   return (
-    <div className="w-[92%] px-1 mx-auto py-20 gap-1 font-manrope">
-      <div className="relative flex flex-col md:flex-row gap-x-6 mx-auto my-6">
+    <div className={cn( "md:w-[92%] w-[95.99%] px-1 mx-auto  font-manrope")}>
+      <div className={`${reverse &&"md:flex-row-reverse pt-6"} "pt-10 relative flex flex-col md:flex-row mx-auto my-6"`}>
         <div
-          className="md:w-[49.999%]"
+          className="md:w-[42.999%]"
           style={{ transitionDelay: "0ms" }}
         >
           <div
-            className="p-4 rounded mt-5 shadow-xl mb-6"
+            className="p-4 rounded mt-5 shadow-xl md:mb-6"
             style={{ background: "rgba(235, 245, 255, 0.2)" }}
           >
             <ul className="list-none">
@@ -38,35 +38,35 @@ const Comp = () => {
           </div>
         </div>
 
-        <div
-          className="relative md:absolute z-1 flex items-center left-1/2 top-[64px] mr-[-28px]"
+        <AboutConnector 
+          
           style={{ transitionDelay: "400ms" }}
         >
           <div
-            className="inline-block p-1 bg-emphasis"
+            className="inline-block p-1 rounded-full"
             style={{ boxShadow: "0 0 0 2px #444D56" }}
-          ></div>
-          <div className="w-10 h-1 bg-gray-300"></div>
+          />
+          <div className="md:w-full max-md:ml-[2px] md:h-1 h-14 w-1 bg-gray-300 relative z-[-1]"/>
           <div
-            className="inline-block p-1 bg-emphasis"
+            className="inline-block p-1 rounded-full"
             style={{ boxShadow: "0 0 0 2px #444D56" }}
-          ></div>
-        </div>
+          />
+        </AboutConnector>
 
         <div
-          className="md:w-[45%']  build-in-scale-right build-in-animate"
+          className="md:w-6/12"
           style={{ transitionDelay: "600ms" }}
         >
           <div
             className="pt-2 pb-1 text-white bg-custom-dark px-4 rounded-t-sm inline-block text-medium"
           >
-            The JBPromend Vision
+            The JBPromend Vision {reverse ? "1.02" : "1.01"}
           </div>
           <div
             className="p-4 rounded-r-sm  shadow-xl mb-6 text-sm"
             style={{ background: "rgba(235, 245, 255, 0.2)" }}
           >
-            <ul className="list-none mb-n4">
+            <ul className="">
               <AboutLi>
                 <div className="flex items-center flex-1">
                   <Svg />
