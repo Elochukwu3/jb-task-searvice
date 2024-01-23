ae66c37dc2ff19f29ec403a71c4f11f79efb015b
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-23 23:31:51 +0100

    clean up

--- app/(company)/(routes)/aboutUs/style.ts
+++ app/(company)/(routes)/aboutUs/style.ts
@@ -7,6 +7,6 @@ import { cn } from "@/lib/utils"
 
 const AboutLi = tw.li`mb-4 flex items-center justify-between 
 `;
-
+const FaqContainer = tw.div`bg-[#ffffff] border-l-2 px-5 py-3 hover:border-[#ffffff] border-black hover:bg-custom-dark hover:text-[#ffffff] transition-colors duration-300 `
 const AboutConnector = tw.div`relative z-1 md:w-[8%] flex max-md:flex-col items-start h-fit md:mt-14 md:items-center`
-export { AboutLi, AboutConnector };
+export { AboutLi, AboutConnector, FaqContainer };
