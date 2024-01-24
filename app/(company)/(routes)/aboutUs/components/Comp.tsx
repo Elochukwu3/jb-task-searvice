2647518807d8a387cac80e190919ca7a65758660
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-24 15:29:07 +0100

    clean up

--- app/(company)/(routes)/aboutUs/components/Comp.tsx
+++ app/(company)/(routes)/aboutUs/components/Comp.tsx
@@ -20,15 +20,7 @@ const Comp = ({reverse}:{reverse?: boolean}) => {
                 style={{ transitionDelay: "200ms" }}
               >
                 <div className="flex items-center">
-                  <svg
-                    aria-hidden="true"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    className="octicon octicon-check-circle-fill text-success mr-2"
-                  >
-                    <path d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12Zm16.28-2.72a.751.751 0 0 0-.018-1.042.751.751 0 0 0-1.042-.018l-5.97 5.97-2.47-2.47a.751.751 0 0 0-1.042.018.751.751 0 0 0-.018 1.042l3 3a.75.75 0 0 0 1.06 0Z"></path>
-                  </svg>
+                  <Svg/>
                   <span className="text-medium">
                     “The only way to do great work is to love what you do”
                   </span>
@@ -58,7 +50,7 @@ const Comp = ({reverse}:{reverse?: boolean}) => {
           style={{ transitionDelay: "600ms" }}
         >
           <div
-            className="pt-2 pb-1 text-white bg-custom-dark px-4 rounded-t-sm inline-block text-medium"
+            className="pt-2 pb-1 text-white bg-custom-greaner px-4 rounded-t-sm inline-block text-medium"
           >
             The JBPromend Vision {reverse ? "1.02" : "1.01"}
           </div>
