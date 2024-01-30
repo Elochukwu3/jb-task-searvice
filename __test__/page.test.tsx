da31dfcd93fdb134ea65c9021b13790349b23e36
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-30 19:45:28 +0100

    error on testing setup

---
+++ __test__/page.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import About from '@app/(company)/(routes)/aboutUs/components/AboutFaq';
+
+describe('Home', () => {
+  it('renders without crashing', () => {
+    render(<About />);
+    expect(screen.getByText("FAQ")).toBeInTheDocument()
+  });
+})
+ 
--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,17 @@
-import type { Config } from 'jest';
-import nextJest from 'next/jest.js';
-
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+ 
 const createJestConfig = nextJest({
   dir: './',
-});
-
+})
+ 
 const config: Config = {
   coverageProvider: 'v8',
-  modulePaths: ["<rootDir>/"],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  testEnvironment: "jsdom",
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-};
-
-export default createJestConfig(config);
-
+  // moduleNameMapper: {
+  //   '^@(.*)$': '<rootDir>/$1',
+  // },
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+ 
+export default createJestConfig(config) 
